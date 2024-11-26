f3453c79d2586a4c11a5ca9d71c93e211a570919
ClaudiuG18 <claudiughise@gmail.com>
2024-11-26 11:12:17 +0100

    added

--- control_poarta.js
+++ control_poarta.js
@@ -1,3 +1,45 @@
+
+$(".close").click(function () {
+  let seconds = 3;
+  $(".not").text("Se inchide...");
+  $(".timer").text(seconds); // Display initial timer value
+  $(".close").prop("disabled", true);
+  $(".open").prop("disabled", true);
+
+  let timer = setInterval(function () {
+    seconds--;
+    if (seconds > 0) {
+      $(".timer").text(seconds); // Update timer value
+    } else {
+      clearInterval(timer);
+      $(".timer").text(""); // Clear timer
+      $(".not").text("");
+      $(".close").prop("disabled", false);
+      $(".open").prop("disabled", false);
+    }
+  }, 1000); // Update every second
+});
+$(".open").click(function () {
+  let seconds = 3;
+  $(".not").text("Se deschide...");
+  $(".timer").text(seconds); // Display initial timer value
+  $(".open").prop("disabled", true);
+  $(".close").prop("disabled", true);
+
+  let timer = setInterval(function () {
+    seconds--;
+    if (seconds > 0) {
+      $(".timer").text(seconds); // Update timer value
+    } else {
+      clearInterval(timer);
+      $(".timer").text(""); // Clear timer
+      $(".not").text("");
+      $(".open").prop("disabled", false);
+      $(".close").prop("disabled", false);
+    }
+  }, 1000); // Update every second
+});
+
 document.addEventListener("DOMContentLoaded", function() {
     function sendCommand(command) {
       fetch(command)
