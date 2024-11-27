09f47b71bd708c7f3797470a1da045fd9c72f1ae
ClaudiuG18 <claudiughise@gmail.com>
2024-11-27 11:58:20 +0100

    start the remake of the personal website

--- pi.claudiu-ghise.de/index.js
+++ pi.claudiu-ghise.de/index.js
@@ -19,22 +19,18 @@ $(document).ready(function () {
 });
 
 $(".close").click(function () {
-  let seconds = 3;
+  let seconds = 2;
   $(".not").text("Se inchide...");
-  $(".timer").text(seconds); // Display initial timer value
   $(".close").prop("disabled", true);
   $(".open").prop("disabled", true);
   turnOffRelay();
-  $(".progress").css("transition", "width 3s ease-in-out");
+  $(".progress").css("transition", "width 2s ease-in-out");
   // Animate the progress bar
-  $(".progress").animate({ width: "100%" }); // Animate over 'seconds'
+  $(".progress").animate({ width: "100%" });
   let timer = setInterval(function () {
     seconds--;
-    if (seconds > 0) {
-      $(".timer").text(seconds); // Update timer value
-    } else {
+    if (seconds < 0) {
       clearInterval(timer);
-      $(".timer").text(""); // Clear timer
       $(".not").text("");
       $(".close").prop("disabled", false);
       $(".open").prop("disabled", false);
@@ -44,20 +40,17 @@ $(".close").click(function () {
 });
 
 $(".open").click(function () {
-  let seconds = 3;
+  let seconds = 2;
   $(".not").text("Se deschide...");
-  $(".timer").text(seconds); // Display initial timer value
   $(".open").prop("disabled", true);
   $(".close").prop("disabled", true);
   turnOnRelay();
   // Animate the progress bar
-  $(".progress").css("transition", "width 3s ease-in-out");
+  $(".progress").css("transition", "width 2s ease-in-out");
   $(".progress").animate({ width: "100%" }); // Animate over 'seconds'
   let timer = setInterval(function () {
     seconds--;
-    if (seconds > 0) {
-      $(".timer").text(seconds); // Update timer value
-    } else {
+    if (seconds < 0) {
       clearInterval(timer);
       $(".timer").text(""); // Clear timer
       $(".not").text("");
